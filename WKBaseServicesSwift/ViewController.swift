243a03a4cfc768ec274ef7102c16be74723da93c
郭伟坤 <guoweikun621@gmail.com>
2016-07-18 16:46:50 +0800

    创建并初始化项目

---
+++ WKBaseServicesSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WKBaseServicesSwift
+//
+//  Created by 郭伟坤 on 16/7/18.
+//  Copyright © 2016年 郭伟坤. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
