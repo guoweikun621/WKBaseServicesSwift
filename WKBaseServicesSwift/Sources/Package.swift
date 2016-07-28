c467482b5db3b6a086504022817e3076472a4d2c
郭伟坤 <guoweikun621@gmail.com>
2016-07-28 15:02:40 +0800

    重构文件目录

---
+++ WKBaseServicesSwift/Sources/Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  WKBaseServicesSwift
+//
+//  Created by 郭伟坤 on 16/7/28.
+//  Copyright © 2016年 郭伟坤. All rights reserved.
+//
+
+//import PackageDescription
+
+//let package = Package(
+//    name: "WKBaseServicesSwift"
+//)
