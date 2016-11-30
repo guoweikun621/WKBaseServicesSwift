76cb90f6e8775179c500aabce5f12434c891c8f1
郭伟坤 <guoweikun621@gmail.com>
2016-11-30 09:40:44 +0800

    添加带有placeholder功能的TextView

---
+++ Sources/View/WKPlaceholderTextView.swift
@@ -0,0 +1,63 @@
+//
+//  WKPlaceholderTextView.swift
+//  WKBaseServicesSwift
+//
+//  Created by 郭伟坤 on 2016/11/30.
+//  Copyright © 2016年 郭伟坤. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+public class WKPlaceholderTextView: UITextView {
+    
+    
+    private var placeholderLabel: UILabel!
+    
+    @IBInspectable public var placeholder: String? {
+        set {
+            placeholderLabel.text = newValue
+        }
+        get {
+            return placeholderLabel.text
+        }
+    }
+    @IBInspectable public var placeholderColor: UIColor? {
+        set {
+            placeholderLabel.textColor = newValue
+        }
+        get {
+            return placeholderLabel.textColor
+        }
+    }
+    
+    override init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        setupView()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+    
+    func setupView() {
+        let inset = self.textContainerInset
+        
+        placeholderLabel = UILabel(frame: CGRect(origin: CGPoint(x: inset.left+2, y: inset.top), size: CGSize(width: self.width - 20, height: 20)))
+        placeholderLabel.font = self.font
+        placeholderLabel.textColor = UIColor.color("cccccc")
+        addSubview(placeholderLabel)
+                
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(textChanged), name: UITextViewTextDidChangeNotification, object: nil)
+    }
+
+    func textChanged(notification: NSNotification) {
+        placeholderLabel.hidden = self.text.lenght > 0
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UITextViewTextDidChangeNotification, object: nil)
+    }
+    
+}
