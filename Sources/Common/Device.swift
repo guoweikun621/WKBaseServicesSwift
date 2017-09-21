3974ab281678dc998215ef3c596dd0610c7ac93e
郭伟坤 <guoweikun621@gmail.com>
2017-09-21 10:25:39 +0800

    更新设备类型判断

--- Sources/Common/Device.swift
+++ Sources/Common/Device.swift
@@ -28,6 +28,45 @@ open class Device: NSObject {
         return ProcessInfo.processInfo.hostName
     }
     
+    
+    
+    /// 设备类型
+    ///
+    /// - iPhone4s: iPhone4，iPhone4s
+    /// - iPhone5E: iPhone5，iPhone5s，iPhone5E
+    /// - iPhone6: iPhone6，iPhone7，iPhone8
+    /// - iPhone6Plus: iPhone6Plus，7Plus，8Plus
+    /// - iPhoneX: iPhone X
+    public enum DeviceType: String {
+        case iPhone4s
+        case iPhone5E
+        case iPhone6
+        case iPhone6Plus
+        case iPhoneX
+    }
+    
+    
+    /// 返回设备类型枚举 enum DeviceType
+    ///
+    /// - Returns: 具体设备类型
+    public class func currentDeviceType() -> DeviceType {
+        let size = (UIScreen.main.bounds.size.width, UIScreen.main.bounds.size.height)
+        switch size {
+        case (320, 480), (480, 320):
+            return .iPhone4s
+        case (320, 568), (568, 320):
+            return .iPhone5E
+        case (375, 667), (667, 375):
+            return .iPhone6
+        case (375, 736), (736, 375):
+            return .iPhone6Plus
+        case (375, 812), (812, 375):
+            return .iPhoneX
+        default:
+            return .iPhone6
+        }
+    }
+    
     /**
      设备屏幕每个点的像素数  scale
      
@@ -44,46 +83,6 @@ open class Device: NSObject {
      */
     public static func size() -> CGSize {
         return UIScreen.main.bounds.size
-    }
-    
-    
-    /// 判断是否 iPhone5 尺寸设备
-    ///
-    /// - Returns: Bool
-    public static func isPhone5() -> Bool {
-        return size().height == 568
-    }
-    
-    
-    /// 判断是否 iPhone4 尺寸设备
-    ///
-    /// - Returns: Bool
-    public static func isPhone4() -> Bool {
-        return size().height == 480
-    }
-    
-    
-    /// 判断是否 iPhone6 尺寸设备
-    ///
-    /// - Returns: Bool
-    public static func isPhone6() -> Bool {
-        return size().height == 667
-    }
-    
-    
-    /// 判断是否 iPhone plus 尺寸设备
-    ///
-    /// - Returns: Bool
-    public static func isPhonePlus() -> Bool {
-        return size().height == 736
-    }
-    
-    
-    /// 判断是否是 iPhone X 设备
-    ///
-    /// - Returns: Bool
-    public static func isPhoneX() -> Bool {
-        return size().height == 812.0
     }
 }
 
@@ -108,5 +107,49 @@ extension Device {
     @available(*, deprecated, message: "请使用 var deviceName: String 属性")
     public static func deviceName() -> String {
         return ProcessInfo.processInfo.hostName
+    }
+    
+    /// 判断是否 iPhone5 尺寸设备
+    ///
+    /// - Returns: Bool
+    @available(*, deprecated, message: "请使用： public class func currentDeviceType() -> DeviceType")
+    public static func isPhone5() -> Bool {
+        return size().height == 568
+    }
+    
+    
+    /// 判断是否 iPhone4 尺寸设备
+    ///
+    /// - Returns: Bool
+    @available(*, deprecated, message: "请使用： public class func currentDeviceType() -> DeviceType")
+    public static func isPhone4() -> Bool {
+        return size().height == 480
+    }
+    
+    
+    /// 判断是否 iPhone6 尺寸设备
+    ///
+    /// - Returns: Bool
+    @available(*, deprecated, message: "请使用： public class func currentDeviceType() -> DeviceType")
+    public static func isPhone6() -> Bool {
+        return size().height == 667
+    }
+    
+    
+    /// 判断是否 iPhone plus 尺寸设备
+    ///
+    /// - Returns: Bool
+    @available(*, deprecated, message: "请使用： public class func currentDeviceType() -> DeviceType")
+    public static func isPhonePlus() -> Bool {
+        return size().height == 736
+    }
+    
+    
+    /// 判断是否是 iPhone X 设备
+    ///
+    /// - Returns: Bool
+    @available(*, deprecated, message: "请使用： public class func currentDeviceType() -> DeviceType")
+    public static func isPhoneX() -> Bool {
+        return size().height == 812.0
     }
 }
